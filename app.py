b8a66dc80ec04d4b2e7f08295c94c44f68e51f4f
Hussein Rashid <113936853+h0-qp@users.noreply.github.com>
2024-03-02 17:37:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -139,5 +139,5 @@ def movie_detail(movie_id):
 #	db.session.commit()
 #	return redirect(url_for('movie_detail', movie_id=movie_id))
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+#if __name__ == '__main__':
+#    app.run(host="0.0.0.0",port=5000)
